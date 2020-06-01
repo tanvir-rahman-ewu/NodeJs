739b346726039ef376a14fa9c6363c7816f13f6c
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-01 21:41:21 +0600

    two way of importing module added

--- Module/module.js
+++ Module/module.js
@@ -1,7 +1,11 @@
 //every individual file in nodejs is a module
 
 console.log(module)
+/* two ways of importing a module */
+//const math = require('./math.js')
 
-const math = require('./math.js')
+//console.log(math.add(10, 10))
 
-console.log(math.add(10, 10))
+const {add} = require('./math')
+
+console.log(add(10, 10))
